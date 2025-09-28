3221d2ebb0d64f2c7e65a2133f8cdd64c00b4257
Reeba Patel <reebapatel2210@gmail.com>
2025-09-28 14:05:21 +0530

    Updated Swap.java with fixes

--- Array_ArrayList/src/Swap.java
+++ Array_ArrayList/src/Swap.java
@@ -1,15 +1,16 @@
 import java.util.Arrays;
 
 public class Swap {
-    static void swap(int[] arr, int i, int j){
+    static void swap(int[] arr, int i, int j) {
         int temp = arr[i];
         arr[i] = arr[j];
         arr[j] = temp;
-    }   
-     public static void main(String[] args) {
-        int[] arr = {12,2,35,43,3};
-        swap(arr,2 ,3);
-System.out.println(Arrays.toString(arr));            
-System.out.println(arr);        
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 12, 2, 35, 43, 3 };
+        swap(arr, 2, 3);
+        System.out.println(Arrays.toString(arr));
+        System.out.println(arr);
     }
 }
