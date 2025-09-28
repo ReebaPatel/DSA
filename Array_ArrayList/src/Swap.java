fa9ca75b6fcf0de75d38e0027a651b91fac0d116
Reeba Patel <reebapatel2210@gmail.com>
2025-09-28 14:02:38 +0530

    swap elements

---
+++ Array_ArrayList/src/Swap.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+public class Swap {
+    static void swap(int[] arr, int i, int j){
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }   
+     public static void main(String[] args) {
+        int[] arr = {12,2,35,43,3};
+        swap(arr,2 ,3);
+System.out.println(Arrays.toString(arr));            
+System.out.println(arr);        
+    }
+}
